198ead64e7322fbb36f78f27cbc53c3149d84600
michaelvolpp <mvolpp89@googlemail.com>
2022-02-27 17:31:43 +0100

    Improve readme.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # The Improved Bayesian Learning Rule for Finite Gaussian Mixtures in TensorFlow 2
 
 ## What is this?
-This is a Tensorflow 2.0 implementation of the improved Bayesian learning rule for Finite Gaussian Mixtures, as proposed by [Lin et al., "Handling the Positive-Definite Constraint in The Bayesian Learning Rule", ICLR 2021](https://arxiv.org/abs/2002.10060).
+This is a Tensorflow 2.0 implementation of the Improved Bayesian Learning Rule for Finite Gaussian Mixtures, as proposed by [Lin et al., "Handling the Positive-Definite Constraint in The Bayesian Learning Rule", ICLR 2021](https://arxiv.org/abs/2002.10060).
 - [Paper by Lin et al.](https://arxiv.org/abs/2002.10060)
 - [Matlab code by original authors](https://github.com/yorkerlin/iBayesLRule)
 
@@ -11,18 +11,16 @@ Plots taken from [the original paper](https://arxiv.org/abs/2002.10060).
 ## Getting Started
 Clone this repository and run
 ```
-pip install TBDTBDTBD
+pip install . 
 ```
 from the source directory.
 
-To check everything works fine, run the tests in folder ```./i_bayes_rule/tests``` by running ```pytest``` from the source directory.
+To check everything works fine, you can run the tests in ```./i_bayes_rule/tests```.
 
-Get familiar: scripts
+To get familiar with the code, have a look at the example script ```./scripts/run_i_bayesian_learning_rule.py```.
 
 ## Notes
-Research code, in development, ...
-
-asserts
+This code is still in development and thus not thoroughly tested. Features may change in the future. It was tested with the packages listed in ```./setup.cfg```.
 
 ## License
 This code is licensed under the MIT license and is free to use by anyone without any restrictions.
--- scripts/run_i_bayesian_learning_rule_gmm.py
+++ scripts/run_i_bayesian_learning_rule_gmm.py
@@ -6,39 +6,34 @@ import tensorflow as tf
 import tensorflow_probability as tfp
 from i_bayes_rule.i_bayesian_learning_rule_gmm import i_bayesian_learning_rule_gmm
 from i_bayes_rule.lnpdf import make_simple_target, make_star_target, make_target
-from aggbyopt_nps.np import create_initial_gmm_parameters
 
 
-# def create_initial_model(n_tasks, d_z, n_components, prior_scale, initial_cov=None):
-#     if np.isscalar(prior_scale):
-#         prior = tfp.distributions.MultivariateNormalDiag(
-#             loc=np.zeros(d_z), scale_identity_multiplier=prior_scale
-#         )
-#     else:
-#         prior = tfp.distributions.MultivariateNormalDiag(
-#             loc=np.zeros(d_z), scale_diag=prior_scale
-#         )
+def create_initial_gmm_parameters(
+    d_z: int,
+    n_tasks: int,
+    n_components: int,
+    prior_scale: float,
+):
+    # TODO: check more sophisticated initializations
+    prior = tfp.distributions.Normal(
+        loc=tf.zeros(d_z), scale=prior_scale * tf.ones(d_z)
+    )
+    initial_cov = prior_scale**2 * tf.eye(d_z)  # same as prior covariance
 
-#     if initial_cov is None:
-#         initial_cov = (
-#             prior.covariance().numpy().astype(np.float32)
-#         )  # use the same initial covariance that was used for sampling the mean
-#     else:
-#         if np.isscalar(initial_cov):
-#             initial_cov = initial_cov * tf.eye(d_z)
+    weights = tf.ones((n_tasks, n_components)) / n_components
+    means = prior.sample((n_tasks, n_components))
+    covs = tf.stack([initial_cov] * n_components, axis=0)
+    covs = tf.stack([covs] * n_tasks, axis=0)
 
-#     weights = np.ones(n_components) / n_components
-#     means = np.zeros((n_components, d_z))
-#     covs = np.zeros((n_components, d_z, d_z))
-#     for i in range(0, n_components):
-#         if n_components == 1:
-#             means[i] = np.zeros(d_z)
-#         else:
-#             means[i] = prior.sample(1).numpy()
-#         # use the same initial covariance that was used for sampling the mean
-#         covs[i] = initial_cov
-
-#     return weights, means, covs
+    # check output
+    assert weights.shape == (n_tasks, n_components)
+    assert means.shape == (n_tasks, n_components, d_z)
+    assert covs.shape == (n_tasks, n_components, d_z, d_z)
+    if n_tasks == 1:  # for backwards compatibility
+        weights = tf.squeeze(weights, 0)
+        means = tf.squeeze(means, 0)
+        covs = tf.squeeze(covs, 0)
+    return weights, means, covs
 
 
 def plot2d(target_dist, model, fig, axes, block=False):
@@ -64,7 +59,7 @@ def plot2d(target_dist, model, fig, axes, block=False):
     else:
         assert tf.rank(model.loc) == 3  # not implemented for more than one batch-dim
         n_tasks = model.loc.shape[0]
-        xy_tf = tf.broadcast_to(xy_tf[:, None, :], (n_plt ** 2, n_tasks, 2))
+        xy_tf = tf.broadcast_to(xy_tf[:, None, :], (n_plt**2, n_tasks, 2))
 
     # evaluate distributions
     p_tgt = np.exp(target_dist.log_density(xy_tf).numpy())
@@ -149,26 +144,25 @@ def main():
     tf.random.set_seed(config["seed"])
 
     ## set number of tasks
-    # TODO: adapt the other toy problems to more than one task!
-    n_tasks = 1
+    n_tasks = 3
 
-    ## Create target dist (GMM): I tested the following target distributions/settings
+    ## Create target dist (GMM): choose one of the following target distributions
     # (i) Simple toy target
-    # config["savepath"] = os.path.join(scriptpath, "log", "toy_gmm")
-    # config["n_components_model"] = 3
-    # config["n_dimensions_model"] = 2
-    # target_dist = make_simple_target(n_tasks=n_tasks)
-    # (ii) Star-GMM from Lin et al. (Khan)
+    config["savepath"] = os.path.join(scriptpath, "log", "toy_gmm")
+    config["n_components_model"] = 3
+    config["n_dimensions_model"] = 2
+    target_dist = make_simple_target(n_tasks=n_tasks)
+    # (ii) Star-GMM from Lin et al.
     # config["savepath"] = os.path.join(scriptpath, "log", "star_gmm")
     # config["n_components_model"] = 10
     # config["n_dimensions_model"] = 2
     # target_dist = make_star_target(num_components=5)
     # (iii) Random GMM
-    config["savepath"] = os.path.join(scriptpath, "log", "random_gmm")
-    n_dimensions = 200
-    config["n_components_model"] = 3
-    config["n_dimensions_model"] = n_dimensions
-    target_dist = make_target(num_dimensions=n_dimensions)
+    # config["savepath"] = os.path.join(scriptpath, "log", "random_gmm")
+    # n_dimensions = 200
+    # config["n_components_model"] = 3
+    # config["n_dimensions_model"] = n_dimensions
+    # target_dist = make_target(num_dimensions=n_dimensions)
 
     ## create initial GMM parameters
     w_init, mu_init, cov_init = create_initial_gmm_parameters(
